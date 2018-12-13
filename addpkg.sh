0cfe0062832d9f4fd0b927aad2d4c8c5f0e3f204
akiq2016 <akiq2016@gmail.com>
2018-12-13 21:57:17 +0800

    chore: tweaks addpkg

--- addpkg.sh
+++ addpkg.sh
@@ -5,5 +5,7 @@ pkgsPath=$rootPath/packages
 
 echo "try to new a $1 package ..."
 cd $pkgsPath
-git clone https://github.com/modojs/miniprogram-custom-component.git $1
+git clone --depth=1 https://github.com/modojs/miniprogram-custom-component.git $1
+rm -rf "$1/.git"
 cd $rootPath
+lerna bootstrap
