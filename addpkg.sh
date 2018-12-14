40672cec49ea9a4fa1248adbf1c0f342fd448fda
akiq2016 <akiq2016@gmail.com>
2018-12-14 17:52:10 +0800

    chore: update addpkg script

--- addpkg.sh
+++ addpkg.sh
@@ -3,9 +3,19 @@
 rootPath=$(pwd)
 pkgsPath=$rootPath/packages
 
-echo "try to new a $1 package ..."
+echo "> try to new a $1 package ..."
 cd $pkgsPath
 git clone --depth=1 https://github.com/modojs/miniprogram-custom-component.git $1
 rm -rf "$1/.git"
+echo ''
+
+echo "> try to update config files ..."
+python3 $rootPath/addpkg.py $pkgsPath/$1
+echo ''
+
+echo "> try to get node_modules ..."
 cd $rootPath
 lerna bootstrap --hoist
+echo ''
+
+echo "> Created successfully!"
