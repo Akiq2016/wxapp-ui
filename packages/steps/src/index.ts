fe5c58588f255aa875dddad0f8dc9e9432d6db95
akiq2016 <akiq2016@gmail.com>
2019-05-07 20:56:35 +0800

    chore: update scripts

--- packages/steps/src/index.ts
+++ packages/steps/src/index.ts
@@ -17,21 +17,4 @@ Component({
       value: 'http://yed-m-website.oss-cn-hangzhou.aliyuncs.com/topic/done.png',
     }
   },
-  lifetimes: {
-    attached() {
-      let that = this;
-      wx.getSystemInfo({
-        success(res) {
-          // eslint-disable-next-line no-console
-          console.log(res);
-          that.showSomeThing('asdasd');
-        }
-      })
-    }
-  },
-  methods: {
-    showSomeThing(title) {
-      console.log('you dont know', title);
-    }
-  }
 });
