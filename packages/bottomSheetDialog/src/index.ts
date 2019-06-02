9ced3fc848fb02de8cf272efa74d4060dbc219b5
akiq2016 <akiq2016@gmail.com>
2019-06-02 20:31:33 +0800

    chore: update tpl

--- packages/bottomSheetDialog/src/index.ts
+++ packages/bottomSheetDialog/src/index.ts
@@ -1,39 +1,9 @@
-
-// ## Properties
-
-// | Name              | Type           | Default            | Description                                       |
-// | :---------------- |:---------------|:-------------------|:--------------------------------------------------|
-// | title             | String         |                    | 底部菜单弹框标题，不填则标题行不显示                    |
-// | contentStyle      | String         | row                | 设置底部菜单弹框内容列表样式。可选参数 ['row', 'column'] |
-// | contentList       | contentItem[]  |                    | 底部菜单弹框内容列表                                  |
-// | dialogStatus      | Boolean        | false              | 设置底部菜单弹框的显示状态                             |
-// | duration          | Number         | 200                | 设置底部菜单弹框出现的过渡时间                          |
-// | bgColor           | String         | rgba(0, 0, 0, 0.5) | 蒙层背景颜色                                         |
-
 Component({
   properties: {
-    title: {
-      type: String
-    },
-    contentStyle: {
+    prop: {
       type: String,
-      value: 'row'
+      value: '你不知道的事情还有很多'
     },
-    contentList: {
-      type: Array
-    },
-    dialogStatus: {
-      type: Boolean,
-      value: false,
-    },
-    duration: {
-      type: Number,
-      value: 200
-    },
-    bgColor: {
-      type: String,
-      value: 'rgba(0, 0, 0, 0.5)'
-    }
   },
   lifetimes: {
     attached() {
@@ -48,7 +18,7 @@ Component({
     }
   },
   methods: {
-    showSomeThing(title: any) {
+    showSomeThing(title) {
       console.log('you dont know', title);
     }
   }
--- packages/bottomSheetDialog/tools/demo/pages/index/index.js
+++
@@ -1,6 +0,0 @@
-Page({
-  data: {
-    title: 'aki tycho',
-    dialogStatus: false,
-  },
-});
